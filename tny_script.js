09beffefffaf934370aa783ab29b62e3de84bf27
Paige Mabbitt <PMabbitt992@west-mec.org>
2019-02-05 15:19:08 -0700

    First Commit
    File editations and script testing

--- tny_script.js
+++ tny_script.js
@@ -1,11 +1,14 @@
+"use strict";
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
    Tutorial Case
 
    Countdown Clock
-   Author: 
-   Date:   
+   Author: Paige Mabbitt
+   Date: 2.5.19
+   
 
 */
 
+// window.alert("Welcome to Tulsa");
